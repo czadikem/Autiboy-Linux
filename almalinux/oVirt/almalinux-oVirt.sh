c217ab2278a23b9c64077e753baa7d6a6e97455a
czadikem <35937408+czadikem@users.noreply.github.com>
2022-04-15 14:12:59 -0600

    Update and rename almalinux-oVirt.sh to almalinux/oVirt/almalinux-oVirt.sh

---
+++ almalinux/oVirt/almalinux-oVirt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Disable Sudo timeout
+# https://gist.github.com/cowboy/3118588
+echo "Enabling Sudo timeout loop"
+sudo -v
+while true; do sleep 60; sudo -n true; kill -0 "$$" || exit; done 2>/dev/null &
+function wait() {
+  echo -n "["; for i in {1..60}; do sleep $1; echo -n =; done; echo "]"
+}
+
+wget 
+
+sudo ./ovirt-repos-setup.sh
+
+sudo rpm -i --justdb --nodeps --force "http://mirror.centos.org/centos/8-stream/BaseOS/$(rpm --eval '%_arch')/os/Packages/centos-stream-release-8.6-1.el8.noarch.rpm"
+
+sudo echo "8-stream" > /etc/yum/vars/stream
+
+sudo dnf distro-sync --nobest
+
+# Stop Sudo timeout loop
+echo "Stopping Sudo timeout loop"
+echo "done."
