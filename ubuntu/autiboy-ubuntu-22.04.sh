e163fd233e2fd6bbe57ed3da8ea8a846d3c80361
czadikem <35937408+czadikem@users.noreply.github.com>
2022-04-25 19:17:48 +0000

    Update autiboy-ubuntu-22.04.sh

--- ubuntu/autiboy-ubuntu-22.04.sh
+++ ubuntu/autiboy-ubuntu-22.04.sh
@@ -195,16 +195,7 @@ sudo mv 50-numworks-calculator-f2be8a48f68f1ee4d88c997c35194960.rules /etc/udev/
 
 # Install VSCodium
 # https://vscodium.com
-# repo from https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo
-echo "Installing VSCodium and it's python modules"
-sleep 5
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-    | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list
-sudo apt update
-sudo apt install codium -y
+sudo snap install codium --classic
 sudo apt install python3-tk -y
 sudo apt install python3-pip -y
 pip3 install setuptools
