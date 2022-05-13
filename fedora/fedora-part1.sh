0b56d084f94fcc97a6b9a9d15bb463623c1d57ca
czadikem <35937408+czadikem@users.noreply.github.com>
2022-05-12 20:19:54 -0600

    Create fedora-part1.sh

---
+++ fedora/fedora-part1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# sleep from
+# https://linuxize.com/post/how-to-use-linux-sleep-command-to-pause-a-bash-script/
+
+# Disable Sudo timeout
+# https://gist.github.com/cowboy/3118588
+echo "Enabling Sudo timeout loop"
+sudo -v
+while true; do sleep 60; sudo -n true; kill -0 "$$" || exit; done 2>/dev/null &
+function wait() {
+  echo -n "["; for i in {1..60}; do sleep $1; echo -n =; done; echo "]"
+}
+
+# Plug in Ventoy USB Drive
+echo "Please insert the Ventoy USB Drive you used to install this OS."
+sleep 20
+
+# Install Updates and Upgrade
+echo "Running dnf upgrade -y"
+sleep 5
+sudo dnf upgrade -y
+
+# Install git
+echo "Installing Git"
+sleep 5
+sudo dnf install git -y
+
+# Install Snap
+echo "Installing Snap"
+sleep 5
+sudo dnf install snapd -y
+
+# Add Flathub repo to Flatpak
+echo "Adding Flathub repo"
+sleep 5
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Add RPM Fusion Free and Non-Free repos
+# https://rpmfusion.org
+echo "Adding RPM Fusion Free and Non-Free repos"
+sleep 5
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+# Install Gnome-Tweaks and Gnome-Shell-Extensions
+echo "Installing Gnome-Tweaks and Gnome-Shell-Extensions"
+sleep 5
+sudo dnf install gnome-tweaks gnome-extensions-app -y
