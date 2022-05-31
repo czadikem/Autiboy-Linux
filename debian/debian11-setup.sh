458ad9e442c3cbdf80ce7aaf69a11248a8fd3b97
czadikem <35937408+czadikem@users.noreply.github.com>
2022-05-30 18:33:37 -0600

    Update debian11-setup.sh

--- debian/debian11-setup.sh
+++ debian/debian11-setup.sh
@@ -125,29 +125,6 @@ pip3 install setuptools
 pip3 install opencv-python
 pip3 install matplotlib
 
-# Enable Microsoft VS Code Marketplace
-# https://stackoverflow.com/a/21549836
-# https://github.com/VSCodium/vscodium/blob/6a86200d383a5ded36e72e47da024429630e253a/DOCS.md#how-to-use-the-vs-code-marketplace
-echo "Enabling Microsoft VS Code Marketplace"
-sleep 5
-wget https://raw.githubusercontent.com/czadikem/autiboys-linux/master/VSCodium-product.json -O /home/autiboy/.config/VSCodium/product.json
-chown autiboy:autiboy /home/autiboy/.config/VSCodium/product.json
-# Install VSCodium Plugins
-echo "Installing VSCodium Plugins"
-sleep 5
-echo "Installing Python Extension"
-codium --install-extension ms-python.python
-echo "Installing Gitlab Workflow Extension"
-codium --install-extension GitLab.gitlab-workflow
-echo "Installing Arduino Extension Pack"
-codium --install-extension mpty.pack-arduino
-
-# Setup VSCodium Git
-echo "Setting up VSCodium Git"
-sleep 5
-git config --global user.name "Autiboy"
-git config --global user.email czadikem@readytodream.com
-
 # Install Gparted
 echo "Installing gparted"
 sleep 5
@@ -182,12 +159,6 @@ echo "Installing Blender"
 sleep 5
 flatpak install flathub org.blender.Blender -y
 
-# Setup Blender
-echo "Setting up Blender"
-sleep 5
-cp -r /home/autiboy/Applications/3.1 /home/autiboy/.var/app/org.blender.Blender/config/blender/
-chown -R autiboy:autiboy /home/autiboy/.var/app/org.blender.Blender/config/blender/3.1
-
 # Install WireGuard
 # https://www.wireguard.com/install/#ubuntu-module-tools
 echo "Installing WireGuard"
@@ -203,18 +174,6 @@ wget https://raw.githubusercontent.com/czadikem/autiboys-linux/master/debian/pee
 echo "Installing NumWorks Calc Driver"
 sleep 5
 wget https://raw.githubusercontent.com/czadikem/autiboys-linux/master/50-numworks-calculator-f2be8a48f68f1ee4d88c997c35194960.rules -P /etc/udev/rules.d/
-
-# Set time to AM/PM for Gnome
-# https://askubuntu.com/a/1183891
-echo "Set time to AM/PM for Gnome"
-sleep 5
-gsettings set org.gnome.desktop.interface clock-format '12h'
-
-# Show bettery percentage in Gnome
-# https://askubuntu.com/a/947875
-echo "Enable Battery Percentage in Gnome"
-sleep 5
-gsettings set org.gnome.desktop.interface show-battery-percentage true
 
 # Rebooting Computer
 echo "Rebooting Computer Now"
