4b9d83c05311e405978d968742aead605de72000
czadikem <35937408+czadikem@users.noreply.github.com>
2022-06-03 20:10:03 -0600

    Update debian11-setup.sh

--- debian/debian11-setup.sh
+++ debian/debian11-setup.sh
@@ -131,9 +131,6 @@ flatpak install flathub org.videolan.VLC -y
 echo "Installing OnlyOffice"
 sleep 5
 flatpak install flathub org.onlyoffice.desktopeditors -y
-echo "Installing Transmission"
-sleep 5
-flatpak install flathub com.transmissionbt.Transmission -y
 echo "Installing Thunderbird"
 sleep 5
 flatpak install flathub org.mozilla.Thunderbird -y
