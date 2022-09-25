240fda97ae4760a5511cfda1daebda1e8b3e3885
czadikem <35937408+czadikem@users.noreply.github.com>
2022-09-24 18:38:46 -0600

    Update debian11-setup.sh

--- debian/debian11-setup.sh
+++ debian/debian11-setup.sh
@@ -163,7 +163,7 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
     | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | tee /etc/apt/sources.list.d/vscodium.list
-nala update && nala install codium
+nala update && nala install codium -y
 
 # Setup VSCodium
 echo "Setting up VSCodium"
