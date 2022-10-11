79d15cbcebee52850523084b6ee02e33b23aea27
czadikem <35937408+czadikem@users.noreply.github.com>
2022-10-11 14:42:43 -0600

    Update debian11-configure.sh

--- debian/debian11-configure.sh
+++ debian/debian11-configure.sh
@@ -43,6 +43,7 @@ sleep 5
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 
 # Open Terminator with CTRL, ALT, t
+# https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'terminal'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Primary><Alt>t'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'terminator'"
