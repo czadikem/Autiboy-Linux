f416c266b5944dd1f088538d59f103c1f3d31691
czadikem <35937408+czadikem@users.noreply.github.com>
2022-10-16 15:09:44 -0600

    Update kde-install.sh

--- almalinux/kde-install.sh
+++ almalinux/kde-install.sh
@@ -5,6 +5,13 @@ echo "Installing KDE"
 sleep 5
 sudo dnf groupinstall "KDE Plasma Workspaces" "Firefox Web Browser" "KDE Applications" "KDE Educational applications" "KDE Multimedia support" -y
 
+# Set default to Graphical Boot
+# https://www.linuxcapable.com/how-to-install-kde-plasma-desktop-on-almalinux-8/
+echo "Setting default to Graphical Boot"
+sleep 5
+echo "exec /usr/bin/startkde" >> ~/.xinitrc
+sudo systemctl set-default graphical
+
 # Download Configure Script
 echo "Downloading Configure Script"
 sleep 5
