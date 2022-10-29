12a4029f7bed93936169eb1c0ddc6b4709358e4f
czadikem <35937408+czadikem@users.noreply.github.com>
2022-10-29 12:51:19 -0600

    Update gnome-install.sh

--- almalinux/gnome-install.sh
+++ almalinux/gnome-install.sh
@@ -20,9 +20,9 @@ dnf install gnome-tweaks -y
 # Download Configure Script
 echo "Downloading Configure Script"
 sleep 5
-wget https://raw.githubusercontent.com/czadikem/autiboys-linux/master/almalinux/almalinux8-configure.sh -P /home/autiboy/
-chmod +x /home/autiboy/almalinux8-configure.sh
-chown autiboy:autiboy /home/autiboy/almalinux8-configure.sh
+wget https://raw.githubusercontent.com/czadikem/autiboys-linux/master/almalinux/almalinux8-gnome-configure.sh -P /home/autiboy/
+chmod +x /home/autiboy/almalinux8-gnome-configure.sh
+chown autiboy:autiboy /home/autiboy/almalinux8-gnome-configure.sh
 
 # Reboot Computer
 echo "Please Reboot your Computer Now"
