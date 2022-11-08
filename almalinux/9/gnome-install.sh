fe42426cec2888de91c37e9b3b8dc12b532ed4b2
czadikem <35937408+czadikem@users.noreply.github.com>
2022-11-08 14:27:00 -0700

    Create gnome-install.sh

---
+++ almalinux/9/gnome-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Gnome
+echo "Installing Gnome"
+sleep 5
+dnf groupinstall "Workstation" "GNOME Applications" -y
+
+# Set default to Graphical Boot
+# https://www.linuxcapable.com/how-to-install-kde-plasma-desktop-on-almalinux-8/
+echo "Setting default to Graphical Boot"
+sleep 5
+echo "exec /usr/bin/startkde" >> ~/.xinitrc
+sudo systemctl set-default graphical
+
+# Install Gnome Tweaks
+echo "Installing Gnome-Tweaks and Gnome-Extensions"
+sleep 5
+dnf install gnome-tweaks gnome-extensions-app -y
+
+# Download Configure Script
+echo "Downloading Configure Script"
+sleep 5
+wget https://raw.githubusercontent.com/czadikem/autiboys-linux/master/almalinux/8/almalinux8-gnome-configure.sh -P /home/autiboy/ -O almalinux8-configure.sh
+chmod +x /home/autiboy/almalinux8-configure.sh
+chown autiboy:autiboy /home/autiboy/almalinux8-configure.sh
+
+# Reboot Computer
+echo "Please Reboot your Computer Now"
