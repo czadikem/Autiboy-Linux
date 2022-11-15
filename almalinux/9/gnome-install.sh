869cfe0bed83354999e2706c9e28aa515d70a176
czadikem <35937408+czadikem@users.noreply.github.com>
2022-11-14 19:29:47 -0700

    Update gnome-install.sh

--- almalinux/9/gnome-install.sh
+++ almalinux/9/gnome-install.sh
@@ -5,6 +5,14 @@ echo "Installing Gnome"
 sleep 5
 dnf groupinstall "Workstation" "GNOME Applications" -y
 
+# Nvidia Drivers
+# https://www.linuxcapable.com/how-to-install-nvidia-drivers-on-almalinux-9/
+echo "Setting up Nvidia Drivers"
+sleep 5
+sudo dnf config-manager --add-repo http://developer.download.nvidia.com/compute/cuda/repos/rhel9/$(uname -i)/cuda-rhel9.repo
+sudo dnf install kernel-headers-$(uname -r) kernel-devel-$(uname -r) tar bzip2 make automake gcc gcc-c++ pciutils elfutils-libelf-devel libglvnd-opengl libglvnd-glx libglvnd-devel acpid pkgconfig dkms -y
+sudo dnf module install nvidia-driver:latest-dkms -y
+
 # Set default to Graphical Boot
 # https://www.linuxcapable.com/how-to-install-kde-plasma-desktop-on-almalinux-8/
 echo "Setting default to Graphical Boot"
