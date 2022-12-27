01a0cb88cf2f7969957d66f9c182fa2a8ebaa915
czadikem <35937408+czadikem@users.noreply.github.com>
2022-12-27 11:42:17 -0700

    Update debian11-server-setup.sh

--- debian/11/debian11-server-setup.sh
+++ debian/11/debian11-server-setup.sh
@@ -54,5 +54,10 @@ echo "Installing portainer"
 sleep 5
 docker run -d --name="portainer" --restart on-failure -p 9000:9000 -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+# Delete Install Scripts
+echo "Deleting Install Scripts"
+sleep 5
+rm -r /home/autiboy/*.sh
+
 # Reboot Computer
 echo "Please Reboot your Computer Now"
