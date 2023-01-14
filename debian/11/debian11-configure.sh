3c9ec53273a0f6d95ffe804a830b1fb3da90d935
czadikem <35937408+czadikem@users.noreply.github.com>
2023-01-14 09:35:04 -0700

    Update debian11-configure.sh

--- debian/11/debian11-configure.sh
+++ debian/11/debian11-configure.sh
@@ -143,7 +143,7 @@ echo "Deleting Install Scripts"
 sleep 5
 rm -r /home/autiboy/*.sh
 rm -r /home/autiboy/*.deb
-rm -r /home/autiboy/Applications.tar.gz
+rm -r /home/autiboy/Applications.tar.xz
 
 # Reboot Computer
 echo "Please Reboot your Computer Now"
