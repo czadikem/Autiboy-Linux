7a776e11f0b16119e4e2013f72a1700d204c75d5
czadikem <35937408+czadikem@users.noreply.github.com>
2023-01-16 13:47:52 -0700

    Update fedora37-setup.sh

--- fedora/37/fedora37-setup.sh
+++ fedora/37/fedora37-setup.sh
@@ -91,10 +91,10 @@ echo "Installing Flatpak"
 sleep 5
 dnf install flatpak -y
 
-# Add Flathub repo to Flatpak
-echo "Adding Flathub repo"
-sleep 5
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+## Add Flathub repo to Flatpak
+#echo "Adding Flathub repo"
+#sleep 5
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install plugins for playing movies and music
 # https://docs.fedoraproject.org/en-US/quick-docs/assembly_installing-plugins-for-playing-movies-and-music/
